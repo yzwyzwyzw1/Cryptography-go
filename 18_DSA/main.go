044089407a98fd670e6cb948c167153909592129
yzwyzwyzw <1957855254@qq.com>
2019-03-29 21:26:54 +0800

    DSA签名示例

--- 18_DSA/main.go
+++ 18_DSA/main.go
@@ -1,1 +1,37 @@
-package _8_DSA
+package main
+
+import (
+	"crypto/dsa"
+	"crypto/rand"
+	"fmt"
+)
+
+
+//DSA是专门用于数字签名的技术
+func main() {
+
+	//设置私钥使用的参数
+	var param dsa.Parameters
+
+	dsa.GenerateParameters(&param,rand.Reader,dsa.L1024N160)
+
+	//创建私钥
+	var pri dsa.PrivateKey
+	pri.Parameters = param
+
+	//生成私钥
+	dsa.GenerateKey(&pri,rand.Reader)
+
+	//由私钥创建公钥
+	pub := pri.PublicKey
+	message := []byte("hello world")
+
+	//签名
+	r,s,_ := dsa.Sign(rand.Reader,&pri,message)
+
+	//公钥验签
+	if dsa.Verify(&pub,message,r,s) {
+		fmt.Println("验证签名成功")
+	}
+
+}
